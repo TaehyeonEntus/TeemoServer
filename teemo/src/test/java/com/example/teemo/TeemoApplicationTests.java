18e358ba3f78460f24d72310f0fd3ef081eda761
KTH <taehyeonentus@gmail.com>
2024-07-02 23:19:14 +0900

    20240702

---
+++ teemo/src/test/java/com/example/teemo/TeemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
